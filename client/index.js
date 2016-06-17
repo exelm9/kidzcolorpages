b72d940bc672ed0eb3405b7fd78f3c9338b57faa
Forrest Akin <forrest.akin@gmail.com>
2016-06-16 17:21:47 -0700

    [add] adds initial react file structure

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './containers/App/App';
+
+ReactDOM.render(
+  <App />
+  , document.querySelector('.container')
+);
---
+++ client/redux/modules/reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import multireducer from 'multireducer';
+import { routerReducer } from 'react-router-redux';
+import {reducer as reduxAsyncConnect} from 'redux-async-connect';
+import {reducer as form} from 'redux-form';
+
+export default combineReducers({
+});

29167a62d44766acc975b9b87a3d01b56abf6222
jobcrackerbah <erik@critiqueit.com>
2016-06-17 13:15:54 -0700

    [add] added a couple dependancies
    also deleted dev conditional from create.js

--- client/index.js
+++ client/index.js
@@ -1,5 +1,16 @@
+// entry point for react app
+
+//full ES2015 environment for application
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createStore from './redux/create';
+// will need api client
+import {Provider} from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { ReduxAsyncConnect } from 'redux-async-connect';
+import useScroll from 'scroll-behavior/lib/useStandardScroll';
 
 import App from './containers/App/App';
 

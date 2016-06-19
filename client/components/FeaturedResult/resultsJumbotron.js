f3771309207eda51c60d02ab40a56f5bca92c97b
Roberto Lewis <roberto.lewis@gmail.com>
2016-06-18 17:55:59 -0700

    [progress] passes data to jumbotron

--- client/components/FeaturedResult/resultsJumbotron.js
+++ client/components/FeaturedResult/resultsJumbotron.js
@@ -1,9 +1,16 @@
 // A display for featured content (functional)
 import React from 'react';
 
-const ResultsJumbotron = () => {
+const ResultsJumbotron = ({ image, caption }) => {
+  console.log("Inside Jumbotron: ", caption);
   return (
     <div>Jumbotron</div>
+    // <div class="item active">
+    //        <img src="..." alt="...">
+    //        <div class="carousel-caption">
+    //          <h3></h3>
+    //        </div>
+    //      </div>
   );
 };
 

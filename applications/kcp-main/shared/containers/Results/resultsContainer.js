1dbaf8ed738e0619835ea6573a26aeebfe3452ed
Forrest Akin <forrest.akin@gmail.com>
2016-06-27 19:36:08 -0700

    [progress] refactors modal for new data

--- applications/kcp-main/shared/containers/Results/resultsContainer.js
+++ applications/kcp-main/shared/containers/Results/resultsContainer.js
@@ -19,10 +19,8 @@ export default class ResultsContainer extends Component {
 
   }
 
-  showModal({imgIdx, uuid}) {
-    console.log(uuid, imgIdx)
-    return false;
-    this.props.actions.showModal({ show: true, imgIdx, results: this.props.categoryList });
+  showModal(uuid, colIdx) {
+    this.props.actions.showModal({ uuid, colIdx, results: this.props.api.collections[uuid].aliases });
   }
 
   render() {

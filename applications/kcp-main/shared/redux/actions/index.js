f6073749cd4c5c8bd3ecbc92b8c5ab76495f3044
Forrest Akin <forrest.akin@gmail.com>
2016-06-28 10:56:22 -0700

    [progress] modal opens and closes, gallery doesn't display

--- applications/kcp-main/shared/redux/actions/index.js
+++ applications/kcp-main/shared/redux/actions/index.js
@@ -29,6 +29,10 @@ export const showModal = (modalState) => {
   return { type: actions.SHOW_MODAL, ...modalState };
 };
 
+export const changeImage = (imgIdx) => {
+  return { type: actions.CHANGE_IMAGE, imgIdx };
+};
+
 export const hideModal = (show) => {
   return { type: actions.HIDE_MODAL, show };
 };

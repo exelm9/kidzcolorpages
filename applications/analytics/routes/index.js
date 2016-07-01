1bc263141c4a148810d5208099884d412740a60e
Forrest Akin <forrest.akin@gmail.com>
2016-07-01 14:54:43 -0700

    [rebase]

---
+++ applications/analytics/routes/index.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+
+// Atlas Paths.
+require(path.join(__dirname, '/../../../autoload'));
+const atlas_paths = require('atlas_paths');
+
+const MA = require('../../media-aliases/media-aliases');
+const index = MA.get_index();
+
+module.exports = function(app){
+
+	// make it easier to work with request date.
+	app.use(bodyParser.json());
+
+	app.get('/analytics/*', function(req, res, next) {
+
+
+		const query = 'aladdin';
+		// const searchFilters; // Filter Results by
+		// const offset; // Return results from this offset
+		// const size; // how many results do you want
+
+		const _index = index  || MA.get_index();
+		// const results = [];
+
+		// var keys = Object.key(index.categories);
+		// key.filter(function(key){
+		// 	if( key.indexOf(query)) return true;
+		// })
+
+		// key.map(function(key){
+		// 	results.push( index.categories[key] )
+		// })
+
+		// const filters = require(path.join(__dirname, '../filters'));
+
+	  	res.json(JSON.stringify(_index.categories))
+	});
+};

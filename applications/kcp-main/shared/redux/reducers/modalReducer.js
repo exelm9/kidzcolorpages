328ba0b69c4b17aa0ebdbef648da72017a7906b4
Forrest Akin <forrest.akin@gmail.com>
2016-07-01 15:19:17 -0700

    [rebase]

--- applications/kcp-main/shared/redux/reducers/modalReducer.js
+++ applications/kcp-main/shared/redux/reducers/modalReducer.js
@@ -12,13 +12,13 @@ const initialState = {
 export default function modalReducer(state = initialState, action) {
   switch (action.type) {
     case SHOW_MODAL:
-      return { ...state, show: true, collectionData: action.collectionData, collections: action.collections, colIdx: action.colIdx, aliases: action.aliases, imgIdx: action.imgIdx };
+      return { ...state, show: true, collectionData: action.collectionData, collections: action.collections, colIdx: action.colIdx, aliases: action.aliases, imgIdx: action.imgIdx, direction: 'left' };
     case HIDE_MODAL:
       return { ...state, show: false };
     case CHANGE_IMAGE:
       return { ...state, imgIdx: action.imgIdx };
     case CHANGE_COLLECTION:
-      return { ...state, colIdx: action.colIdx, aliases: action.aliases, imgIdx: 0 };
+      return { ...state, colIdx: action.colIdx, aliases: action.aliases, imgIdx: 0, direction: action.direction };
     default:
       return state;
   }

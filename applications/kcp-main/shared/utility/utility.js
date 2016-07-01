d98afc4e184c0e0852e404b1d5e512e48a353e50
jobcrackerbah <erik@critiqueit.com>
2016-07-01 13:05:19 -0700

    [Add] Adds picture data to filter

---
+++ applications/kcp-main/shared/utility/utility.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+
+exports.prepareFilterData = function(filters, categories){
+  return _.mapValues(filters, (pages, filter, filters) => {
+  	let reformatedFilter = {};
+  	for(var category in categories){
+  		let categoryFilter = category.split('/').pop()
+  		if(categoryFilter === filter){
+  			let collections = categories[category].collections;
+  			console.log(collections, filter)
+  			reformatedFilter['pictures'] = collections[Object.keys(collections)[0]].collection_thumb;
+  		}
+  	}
+  	reformatedFilter['pages'] = pages
+  	return reformatedFilter;
+  });
+}

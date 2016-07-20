0a3c0fcfd7ed5a3eeb872abd9fea244fa085f4f2
exelm9 <erik@critiqueit.com>
2016-07-20 09:50:34 -0700

    adds config environment variables

--- applications/atlas/app.js
+++ applications/atlas/app.js
@@ -7,6 +7,8 @@ require('babel-register')({})
 // require('babel-polyfill')
 
 // Launch the Server.
+require('./config/config.js');
+console.log(process.env.PRODUCTION,'production var')
 process.env.ATLAS = true;
 process.env.NODE_ENV = 'production';
 exports = require(path.join(__dirname, './server.jsx')).default
---
+++ applications/atlas/config/config.example.js
@@ -0,0 +1,1 @@
+process.env['PRODUCTION'] = false;
